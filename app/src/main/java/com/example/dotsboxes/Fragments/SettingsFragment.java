861cdf33e29cdb0b6628ceabaa69e111c67932da
Diana Nguyen <nguyendh24@vcu.edu>
2022-03-18 00:35:45 -0400

    Help & Setting buttons hidden in settings > toggle button updated for player color

--- app/src/main/java/com/example/dotsboxes/Fragments/SettingsFragment.java
+++ app/src/main/java/com/example/dotsboxes/Fragments/SettingsFragment.java
@@ -18,6 +18,10 @@ import androidx.fragment.app.FragmentManager;
 import com.example.dotsboxes.MainActivity;
 import com.example.dotsboxes.R;
 import com.example.dotsboxes.databinding.FragmentSettingsBinding;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.button.MaterialButtonToggleGroup;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.HashMap;
 
 public class SettingsFragment extends Fragment {
@@ -45,8 +49,11 @@ public class SettingsFragment extends Fragment {
         editor = sharedPreferences.edit();
         settingsView = binding.getRoot();
         Button btnBack = settingsView.findViewById(R.id.btnBack);
-        isPlayer2 = ((SwitchCompat) settingsView.findViewById(R.id.switchColor)).isChecked();
+        MaterialButtonToggleGroup btnToggle = settingsView.findViewById(R.id.btnToggle);
+        MaterialButton btnPlayer2 = settingsView.findViewById(R.id.btnPlayer2);
+        isPlayer2 = btnPlayer2.isChecked();
 
+        hideFloatingBtn(true);
         setRadioGrid();
         setRadioVertices();
         setRadioPlayerColor();
@@ -56,14 +63,16 @@ public class SettingsFragment extends Fragment {
         radioVertices.setOnCheckedChangeListener(getListenerRadioVertices);
         radioColorA.setOnCheckedChangeListener(getListenerRadioColorA);
         radioColorB.setOnCheckedChangeListener(getListenerRadioColorB);
-        switchPlayer.setOnCheckedChangeListener(getListenerSwitchPlayer);
+//        switchPlayer.setOnCheckedChangeListener(getListenerSwitchPlayer);
         btnBack.setOnClickListener(getListenerBtnBack);
+        btnToggle.addOnButtonCheckedListener(getListenerBtnToggle);
 
         return settingsView;
     }
 
     private final View.OnClickListener getListenerBtnBack = view -> {
         FragmentManager fragmentManager = getParentFragmentManager();
+        hideFloatingBtn(false);
         fragmentManager.popBackStack();
     };
 
@@ -156,6 +165,15 @@ public class SettingsFragment extends Fragment {
         }
     };
 
+    private final MaterialButtonToggleGroup.OnButtonCheckedListener getListenerBtnToggle = new MaterialButtonToggleGroup.OnButtonCheckedListener() {
+        @Override
+        public void onButtonChecked(MaterialButtonToggleGroup group, int checkedId, boolean isChecked) {
+            int btnID = group.getCheckedButtonId();
+            isPlayer2 = btnID == R.id.btnPlayer2;
+            setRadioPlayerColor();
+        }
+    };
+
     private void setRadioGrid() {
         radioGrid = settingsView.findViewById(R.id.radioGrid);
         RadioButton rbGrid4 = settingsView.findViewById(R.id.rbGrid4);
@@ -182,7 +200,7 @@ public class SettingsFragment extends Fragment {
     }
 
     private void setRadioPlayerColor() {
-        setSwitchPlayer();
+        setToggle();
         radioColorA = settingsView.findViewById(R.id.radioColorA);
         radioColorB = settingsView.findViewById(R.id.radioColorB);
         RadioButton rbColorBlue = settingsView.findViewById(R.id.rbColorBlue);
@@ -217,9 +235,27 @@ public class SettingsFragment extends Fragment {
         }
     }
 
-    private void setSwitchPlayer() {
-        switchPlayer = settingsView.findViewById(R.id.switchColor);
-        TextView tvPlayerColor = settingsView.findViewById(R.id.tvPlayerColor);
-        if (isPlayer2) { tvPlayerColor.setText("Color for Player 2"); } else { tvPlayerColor.setText("Color for Player 1");}
+    private void setToggle() {
+        MaterialButton btnPlayer1 = settingsView.findViewById(R.id.btnPlayer1);
+        MaterialButton btnPlayer2 = settingsView.findViewById(R.id.btnPlayer2);
+        if (isPlayer2) { btnPlayer2.setChecked(true); } else { btnPlayer1.setChecked(true); }
     }
+
+    private void hideFloatingBtn(boolean isHide) {
+        FloatingActionButton fabHelp = (FloatingActionButton)getActivity().findViewById(R.id.btnHelp);
+        FloatingActionButton fabSettings = (FloatingActionButton)getActivity().findViewById(R.id.btnSettings);
+        if (isHide) {
+            fabHelp.hide();
+            fabSettings.hide();
+        } else {
+            fabHelp.show();
+            fabSettings.show();
+        }
+
+    }
+//    private void setSwitchPlayer() {
+////        switchPlayer = settingsView.findViewById(R.id.switchColor);
+//        TextView tvPlayerColor = settingsView.findViewById(R.id.tvPlayerColor);
+//        if (isPlayer2) { tvPlayerColor.setText("90dpr 2"); } else { tvPlayerColor.setText("Color for Player 1");}
+//    }
 }
