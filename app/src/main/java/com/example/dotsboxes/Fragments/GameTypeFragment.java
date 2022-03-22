dd388e2bc86df3db5847e7647704d866c854d71b
Zachary Fowler <fowlerzw@vcu.edu>
2022-03-22 18:51:09 -0400

    added star animations
    Added vectors and star animations to the GameTypeFragment so the stars having a twinkling effect

--- app/src/main/java/com/example/dotsboxes/Fragments/GameTypeFragment.java
+++ app/src/main/java/com/example/dotsboxes/Fragments/GameTypeFragment.java
@@ -9,7 +9,10 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.dotsboxes.GameState;
 import com.example.dotsboxes.MainActivity;
@@ -35,13 +38,72 @@ public class GameTypeFragment extends Fragment {
         btnPlayPerson.setAlpha(0f);
         btnPlayPerson.setTranslationY(50);
         btnPlayPerson.animate().alpha(1f).translationYBy(-50).setDuration(1500);
+
         Button btnPlayComputer = homeView.findViewById(R.id.btnPlayComputer);
         //add sliding animation to buttons
         btnPlayComputer.setAlpha(0f);
         btnPlayComputer.setTranslationY(50);
         btnPlayComputer.animate().alpha(1f).translationYBy(-50).setDuration(1500);
+
         btnPlayPerson.setOnClickListener(view -> newGame(false));
         btnPlayComputer.setOnClickListener(view -> newGame(true));
+
+        //animations for star background
+        ImageView star1 = homeView.findViewById(R.id.star1);
+        ImageView star2 = homeView.findViewById(R.id.star2);
+        ImageView star3 = homeView.findViewById(R.id.star3);
+        ImageView star4 = homeView.findViewById(R.id.star4);
+        ImageView star5 = homeView.findViewById(R.id.star5);
+        ImageView star6 = homeView.findViewById(R.id.star6);
+        ImageView star7 = homeView.findViewById(R.id.star7);
+        ImageView star8 = homeView.findViewById(R.id.star8);
+        ImageView star9 = homeView.findViewById(R.id.star9);
+        ImageView star10 = homeView.findViewById(R.id.star10);
+        ImageView star11 = homeView.findViewById(R.id.star11);
+        ImageView star12 = homeView.findViewById(R.id.star12);
+        ImageView star13 = homeView.findViewById(R.id.star13);
+        ImageView star14 = homeView.findViewById(R.id.star14);
+        ImageView star15 = homeView.findViewById(R.id.star15);
+        ImageView star16 = homeView.findViewById(R.id.star16);
+        ImageView star17 = homeView.findViewById(R.id.star17);
+        ImageView star18 = homeView.findViewById(R.id.star18);
+        ImageView star19 = homeView.findViewById(R.id.star19);
+        ImageView star20 = homeView.findViewById(R.id.star20);
+        ImageView star21 = homeView.findViewById(R.id.star21);
+        ImageView star22 = homeView.findViewById(R.id.star22);
+        ImageView star23 = homeView.findViewById(R.id.star23);
+        ImageView star24 = homeView.findViewById(R.id.star24);
+        ImageView star25 = homeView.findViewById(R.id.star25);
+
+        Animation twinkle1 = AnimationUtils.loadAnimation(getActivity(), R.anim.star1anim);
+        Animation twinkle2 = AnimationUtils.loadAnimation(getActivity(), R.anim.star2anim);
+        Animation twinkle3 = AnimationUtils.loadAnimation(getActivity(), R.anim.star3anim);
+
+        star1.startAnimation(twinkle1);
+        star2.startAnimation(twinkle2);
+        star3.startAnimation(twinkle3);
+        star4.startAnimation(twinkle1);
+        star5.startAnimation(twinkle2);
+        star6.startAnimation(twinkle3);
+        star7.startAnimation(twinkle1);
+        star8.startAnimation(twinkle2);
+        star9.startAnimation(twinkle3);
+        star10.startAnimation(twinkle1);
+        star11.startAnimation(twinkle2);
+        star12.startAnimation(twinkle3);
+        star13.startAnimation(twinkle1);
+        star14.startAnimation(twinkle2);
+        star15.startAnimation(twinkle3);
+        star16.startAnimation(twinkle1);
+        star17.startAnimation(twinkle2);
+        star18.startAnimation(twinkle3);
+        star19.startAnimation(twinkle1);
+        star20.startAnimation(twinkle2);
+        star21.startAnimation(twinkle3);
+        star22.startAnimation(twinkle1);
+        star23.startAnimation(twinkle2);
+        star24.startAnimation(twinkle3);
+        star25.startAnimation(twinkle1);
 
         return homeView;
     }
